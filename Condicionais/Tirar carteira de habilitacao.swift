d172e14f5ed16d526aba52a43880c7f99f561cb2
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-02-09 19:26:45 -0300

    Create Tirar carteira de habilitacao.swift

---
+++ Condicionais/Tirar carteira de habilitacao.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+var idade: Int = 24
+
+if idade < 18 {
+  print("Voce nao pode tirar carteira de habilitacao ")
+  
+}else{
+  
+    print("Voce pode tirar carteira de habilitacao ")
+}
