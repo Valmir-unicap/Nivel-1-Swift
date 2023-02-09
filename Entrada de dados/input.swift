de4004019c7eb949650186dbacaaa43765c8fd16
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-02-09 19:45:08 -0300

    Create input.swift

---
+++ Entrada de dados/input.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+print("Entrada de dados")
+print("Digite sua idade: ")
+
+let numero = readLine()
+
+print("Sua idade: \(numero!)")
+print("")
+print("Digite seu nome: ")
+
+let name = readLine()
+
+print("")
+print("Ola \(name!)")
