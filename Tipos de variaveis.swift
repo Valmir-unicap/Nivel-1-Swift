9905e13309a1a9ecfeb1a1326a4fe6107892ce3c
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-02-09 18:41:00 -0300

    Create Tipos de variaveis.swift

---
+++ Tipos de variaveis.swift
@@ -0,0 +1,6 @@
+import Foundation
+var nome : String = "Brian"
+var situacao : Bool = true
+var altura : Float = 1.78
+var idade: Int = 24
+print("Ola ",nome, ", sua situacao = ", situacao, ", sua altura = ",altura, ", sua idade = ",idade)
