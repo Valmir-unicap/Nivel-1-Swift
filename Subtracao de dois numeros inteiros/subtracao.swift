4af1ea499d330bec7b1f358d3703bacb3e727314
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-02-10 13:56:58 -0300

    Create subtracao.swift

---
+++ Subtracao de dois numeros inteiros/subtracao.swift
@@ -0,0 +1,9 @@
+import Foundation
+print("Subtracao")
+print("")
+print("Digite o primeiro numero: ")
+var numero1 = Int(readLine()!)!
+print("Digite o segundo numero: ")
+var numero2 = Int(readLine()!)!
+var subtracao = numero1 - numero2
+print("Resultado = ",subtracao)
