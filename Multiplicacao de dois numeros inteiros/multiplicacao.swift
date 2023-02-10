ea65096b175373a532f5403d24f9d0af0dff97d9
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-02-10 14:00:59 -0300

    Create multiplicacao.swift

---
+++ Multiplicacao de dois numeros inteiros/multiplicacao.swift
@@ -0,0 +1,9 @@
+import Foundation
+print("Multiplicacao")
+print("")
+print("Digite o primeiro numero: ")
+var numero1 = Int(readLine()!)!
+print("Digite o segundo numero: ")
+var numero2 = Int(readLine()!)!
+var multiplicacao = numero1 * numero2
+print("Resultado = ",multiplicacao)
