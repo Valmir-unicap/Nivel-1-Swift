383e3113e8fb47d4915f68b264a30b334b5cf0f7
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-02-10 13:58:17 -0300

    Create divisao.swift

---
+++ Divisao de dois numeros inteiros/divisao.swift
@@ -0,0 +1,9 @@
+import Foundation
+print("Divisao")
+print("")
+print("Digite o primeiro numero: ")
+var numero1 = Int(readLine()!)!
+print("Digite o segundo numero: ")
+var numero2 = Int(readLine()!)!
+var divisao = numero1 / numero2
+print("Resultado = ",divisao)
