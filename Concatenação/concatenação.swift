eb4f38eec7a943b4e44a4aa2ad1aa1ecefd6f4c9
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-02-11 12:03:14 -0300

    Create concatenação.swift

---
+++ Concatenação/concatenação.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+print("Xcode")
+print("")
+let apple = 7
+let orange = 3
+let fruts = "As frutas disponiveis são: \(apple) e \(orange)."
+print("Total de frutas = \(apple + orange)")
