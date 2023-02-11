6e23a24bdba0e9f8a54846f025ef9e82b5f4a419
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-02-11 13:30:35 -0300

    Update Função que retorna uma String.swift

--- Função/Função que retorna uma String.swift
+++ Função/Função que retorna uma String.swift
@@ -5,10 +5,10 @@ print("")
 print("Função")
 print("")
 
-let recebe = conteudo(Person: "valmir", esport: "formula 1")
-print(recebe)
-
 func conteudo(Person: String , esport: String) -> String{ //função que retorna uma String
-    return "O esporte favorite é : \(esport)"
+    return "O esporte favorito é : \(esport)"
 }
-    
+
+let recebe = conteudo(Person: "valmir", esport: "formula 1")
+
+print(recebe)
