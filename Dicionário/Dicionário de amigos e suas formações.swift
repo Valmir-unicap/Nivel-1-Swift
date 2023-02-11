d20b256754b10b3b2b928ee0aa0a753db200127c
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-02-11 12:24:57 -0300

    Create Dicionário de amigos e suas formações.swift

---
+++ Dicionário/Dicionário de amigos e suas formações.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+print("Xcode")
+print("")
+print("Dicionário - Amigos e suas formações academicas")
+print("")
+
+var occupation = [
+    "Andre" : "Engenheiro agricola",
+    "Marcus" : "Jornalismo",
+    "Izaque": "Teatro",
+    "Cayo" : "Educação física",
+    "João" : "Biologo"
+]
+
+print(occupation)
