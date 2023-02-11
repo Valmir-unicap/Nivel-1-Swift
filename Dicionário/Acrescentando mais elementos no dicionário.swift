9496045d3af6a2526658d4ecde388d09deba530e
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-02-11 12:29:54 -0300

    Create Acrescentando mais elementos no dicionário.swift

---
+++ Dicionário/Acrescentando mais elementos no dicionário.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+print("Xcode")
+print("")
+print("Dicionário - Amigos e suas formações academicas")
+print("")
+
+var occupation = [
+    "Andre" : "Engenheiro agricola",
+    "Marcus" : "Jornalismo",
+    "Izaque": "Teatro",
+    "Cayo" : "Educação física",
+    "João" : "Biologo"
+]
+
+print(occupation)
+
+print("")
+print("Adicionando mais uma pessoa no dicionário: ")
+occupation ["Ana"] = "Enfermagem"
+
+print("")
+print("Dicionário atualizado: ")
+print(occupation)
+
