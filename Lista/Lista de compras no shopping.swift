976f7b23716df5a167b13cecb10407bf7e935d43
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-02-11 12:17:33 -0300

    Create Lista de compras no shopping.swift

---
+++ Lista/Lista de compras no shopping.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+print("Xcode")
+print("")
+print("Lista de compras no shopping")
+print("")
+
+var shoppingListaCompras = ["3- Bermudas","2- Camisa Polo", "1- Calça jeans" ,"1- Tenis All Star", "1- Par de meias"]
+print(shoppingListaCompras)
+
+print("")
+print("Quarto item da lista é: ")
+print(shoppingListaCompras[3])
+
+print("")
+print("Quero substituir o quarto item da lista para mochila adidas")
+shoppingListaCompras [3] = "Mochila adidas"
+
+print("")
+print("Nova lista = \(shoppingListaCompras)")
